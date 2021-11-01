cd65284a9164d7dd27ef2f8225991cdd32b10047
salpreh <salva.perez46@gmail.com>
2021-11-01 19:11:43 +0100

    Base project template

---
+++ src/main/java/com/salpreh/productsmd/ProductsMdApplication.java
@@ -0,0 +1,13 @@
+package com.salpreh.productsmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsMdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsMdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salpreh/productsmd/ProductsMdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salpreh.productsmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsMdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

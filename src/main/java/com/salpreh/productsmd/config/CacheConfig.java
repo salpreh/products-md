1289263b31ab8df6d5686005153a0eeee2ceab31
salpreh <salva.perez46@gmail.com>
2022-02-20 22:30:09 +0100

    Cache powered by Redis

--- src/main/java/com/salpreh/productsmd/config/CacheConfig.java
+++ src/main/java/com/salpreh/productsmd/config/CacheConfig.java
@@ -1,17 +1,58 @@
 package com.salpreh.productsmd.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.DefaultBaseTypeLimitingValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
 
-    @Bean
+    @Value("${products-md.cache.ttl-seconds}")
+    private Long ttlSecons;
+
+    /**
+     * @deprecated In-memory cache configuration. Moved to redis cache.
+     */
+    @Deprecated
     public CacheManager cacheManager() {
         return new ConcurrentMapCacheManager("products");
+    }
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofSeconds(ttlSecons))
+            .disableCachingNullValues()
+            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                new GenericJackson2JsonRedisSerializer(serializerObjectMapper())
+            ));
+    }
+
+    private ObjectMapper serializerObjectMapper() {
+        var objectMapper = new ObjectMapper();
+
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.activateDefaultTyping(
+            objectMapper.getPolymorphicTypeValidator(),
+            ObjectMapper.DefaultTyping.NON_FINAL,
+            JsonTypeInfo.As.PROPERTY
+        );
+
+        return objectMapper;
     }
 }
